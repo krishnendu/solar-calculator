803d75641181bd1aa6983699522e09b8d58f2563
kris-07 <krishnenduchatterjee25@gmail.com>
2021-06-10 12:26:40 +0530

    commit 2

---
+++ _nuxt/static/1623307651/calculate/load/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsolar-calculator\u002F_nuxt\u002Fstatic\u002F1623307651",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcalculate\u002Fload",config:{_app:{basePath:"\u002Fsolar-calculator\u002F",assetsPath:"\u002Fsolar-calculator\u002F_nuxt\u002F",cdnURL:a}}}}(null));
